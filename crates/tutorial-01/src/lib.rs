8a6b63c1984bfd02bd84b0b09e1323c136fd3da0
Rudra <r82941037@gmail.com>
2022-11-02 08:14:06 +0000

    [HelloWorld] tutorial_01

--- crates/tutorial-01/src/lib.rs
+++ crates/tutorial-01/src/lib.rs
@@ -1,14 +1,22 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// src/lib.rs
 
+wai_bindgen_rust::export!("hello-world.wai");
+
+pub struct HelloWorld;
+
+impl hello_world::HelloWorld for HelloWorld {
+    fn add(a: u32, b: u32) -> u32 {
+        a + b
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
+    use hello_world::HelloWorld as _;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
+    fn add_two_numbers() {
+        let result = HelloWorld::add(2, 2);
         assert_eq!(result, 4);
     }
 }
